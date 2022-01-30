0b905c2b15193037a18d117c839dc0d8049741c5
_smcow <phw2088@gmail.com>
2022-01-30 21:59:53 +0900

    20220130

--- baekjoon/p1436.cpp
+++ baekjoon/p1436.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-
+int Findseries(int N)
+{
+    int i = 666;
+    int series = 0;
+    string target;
+    while(1)
+    {
+        target = to_string(i);
+        for(int j = 0; j < target.length()-2; j++)
+            if(target[j] == '6' && target[j+1] == '6' && target[j+2] == '6')
+            {
+                series++;
+                if(series == N)
+                    return i;
+                break;
+            }
+        i++;
+    }
+}
 int main() {
-  int N;
-  cin >> N;
-  int cnt = 0;
-  while(cnt != N) {
-
-  }
-  return 0;
+    int N;
+    cin >> N;
+    cout << Findseries(N);
 }

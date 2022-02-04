7b9cd89d63ee1539e90605cf6ba6190754d995b7
_smcow <phw2088@gmail.com>
2022-02-04 21:04:44 +0900

    20220204

--- baekjoon/p11650.cpp
+++ baekjoon/p11650.cpp
@@ -2,17 +2,18 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int main(void){
-    int N;
-    cin>>N;
-    vector<vector<int>> arr(N, vector<int>(2,0));
-    for(int i = 0; i < N; i++){
-        cin>>arr[i][1];
-        cin>>arr[i][0];
-    }
-    sort(arr.begin(),arr.end());
-
-    for(int i = 0; i < arr.size(); i++){
-        cout<<arr[i][1]<<" "<<arr[i][0]<<'\n';
-    }
+vector<pair<int, int>> v;
+int main() {
+	int N;
+	cin >> N;
+	int x, y;
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < N; i++) {
+		cout << v[i].first << ' ' << v[i].second << '\n';
+	}
+	return 0;
 }

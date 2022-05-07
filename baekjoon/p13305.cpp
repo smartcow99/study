b45116d10491c9c4f8e9d6c5ab4ef384492bb7cf
_smcow <phw2088@gmail.com>
2022-05-07 22:35:59 +0900

    20220507 node js 공부 1일차

---
+++ baekjoon/p13305.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+
+int main() {
+  cin >> N;
+  vector<int> k;
+  for(int i=0; i<N; i++) {
+
+  }
+  return 0;
+}

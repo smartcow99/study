d9b7561c316aa69247435ac76e984b5a2a7fede0
_smcow <phw2088@gmail.com>
2022-05-07 22:36:29 +0900

    20220507 node js 공부 1일차

---
+++ nodejs/path.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+const string = __filename;
+
+console.log('path.sep:', path.sep);
+
+console.log('path.delimiter:', path.delimiter);
+console.log('path.dirname():', path.dirname);
